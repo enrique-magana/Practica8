cc077c89af2661d0c29adac891cae66f664ab76d
enrique-magana <54093166+enrique-magana@users.noreply.github.com>
2020-05-11 21:25:15 -0500

    Add files via upload

---
+++ circular/node.h
@@ -0,0 +1,19 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <stdio.h>
+
+typedef int DATA;
+
+typedef struct _node node;
+
+struct _node
+{
+	DATA data;
+	node *next;
+	node *prev;
+};
+
+node *create_node(DATA d);
+void remove_node(node *n);
+#endif
